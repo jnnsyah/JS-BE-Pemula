c55105ae4643668f756a954c5875eb8b533badf2
jiansyah <jiansyah1248@gmail.com>
2025-08-01 00:08:49 +0700

    mengubah header dan body request menjadi JSON type

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,21 +1,27 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Type', 'application/json');
     response.statusCode = 200;
 
     const { method, url } = request;
  
     if(url === '/') {
         if(method === 'GET') {
-            response.end('<h1>Ini adalah homepage</h1>');
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
         } else {
             response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
         }
     } else if(url === '/about') {
         if(method === 'GET') {
-            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }))
         } else if(method === 'POST') {
             let body = [];
     
@@ -26,26 +32,27 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
-                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
             });
         } else {
             response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
         }
     } else {
         response.statusCode = 400;
-        response.end('<h1>Halaman tidak ditemukan!</h1>');
+        response.end(JSON.stringify({
+            message: `Halaman tidak ditemukan!`,
+        }));
     }
 };
 
-// curl -X PUT http://localhost:5000/about -i
+// curl -X PUT http://localhost:5000/about
 /* Output :
-HTTP/1.1 400 Bad Request
-Content-Type: text/html
-Date: Thu, 31 Jul 2025 16:44:18 GMT
-Connection: keep-alive
-Keep-Alive: timeout=5
-Content-Length: 60
+{"message":"Halo! Ini adalah halaman about"}
 */
 
 const server = http.createServer(requestListener);
